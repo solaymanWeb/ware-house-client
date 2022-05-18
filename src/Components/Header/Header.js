b805eb1a4c68750d7c7e7f489a5b6798758c8870
solayman1e <solaymanweb27@gmail.comm>
2022-05-18 18:57:32 +0600

    navbar added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+
+const Header = () => {
+
+
+
+    return (
+        <div className='header'>
+     <Navbar bg="" expand="lg">
+    <Container>
+    <Navbar.Brand href="#">
+        <strong>Fridge House</strong>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+        <Nav
+        className="ms-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Link to='/'>Home</Link>
+        <Link to="/blog">Blog</Link>
+
+
+
+        {/* <Nav.Link to='/'>Home</Nav.Link>
+        <Nav.Link to="/blog">Blog</Nav.Link> */}
+
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Header;
