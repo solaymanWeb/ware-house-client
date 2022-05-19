6ebe3522df0e7b79fdd19e9cd71152d6e3d1543e
solayman1e <solaymanweb27@gmail.comm>
2022-05-19 09:09:31 +0600

    six item added Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import banner from '../Images/banner.jpg';
 import './Home.css';
 
 const Home = () => {
+    const [fridges, setFridges]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res =>res.json())
+        .then(data => setFridges(data));
+    },[])
+
+
     return (
         <div >
             {/* BANNER AREA  */}
+
             <Container className='home-container'>
             <Row>
                 <Col><img src={banner} alt="" /></Col>
             </Row>
             </Container>
+
+            {/* PRODUCT AREA */}
+
+            <Container className='services-container' >
+            <Row className="g-5">
+                {
+                    fridges.map(fridge => <Col lg={4}>
+                        <div className='single-fridge'>
+                            <img src={fridge.picture} alt="" />
+                            <h6>Name : {fridge.name}</h6>
+                            <p>Price : {fridge.price}</p>
+                            <p>Quantity : {fridge.quantity}</p>
+                            <p>Suplier : {fridge.supplier}</p>
+                            <p>Discription : {fridge.discription}</p>
+                            <Button variant="success">Update Quantity</Button>
+                        </div>
+                    </Col> )
+                }  
+            </Row>
+            </Container>
+
         </div>
     );
 };
